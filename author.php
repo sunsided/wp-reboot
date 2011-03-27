8be86d5d404763ce37aa166e8daa79c0fe729537
Markus Mayer <development@widemeadows.de>
2011-03-27 20:33:51 +0200

    Author-, Kategorie- und Datumsarchive

--- author.php
+++ author.php
@@ -4,16 +4,24 @@
 
   get_header();
 
-
   if(isset($_GET['author_name'])) :
     $curauth = get_user_by('slug', $author_name);
   else :
     $curauth = get_userdata(intval($author));
   endif;
 
-  $dwooParams['archive']['searchterm'] = $curauth->display_name;
+  $searchterm = $curauth->display_name;
 
-  $dwoo->output(TPL_PATH.'/author.tpl', $dwooParams);
+?>
+
+<div id="searchresult_header" class="searchresult_header">
+<h2 class="searchresult_header">Ergebnisse der Authorensuche</h2>
+<p>Du hast den Blog nach dem Author <strong><?php echo $searchterm; ?></strong> durchsucht. Hier ist, was sich finden ließ.</p>
+</div>
+<div class="separate"></div>
+
+<?php
+
   include(TEMPLATEPATH.'/theloop.php');
 
   get_footer();
--- date.php
+++ date.php
@@ -5,24 +5,39 @@
   get_header();
 
   if (is_year ()) {
-    $dwooParams['archive']['searchterm'] = get_the_date(__('Y'));
+    $searchterm = get_the_date(__('Y'));
   }
   elseif (is_month()) {
-      $dwooParams['archive']['searchterm'] = get_the_date(__('F Y'));
+      $searchterm = get_the_date(__('F Y'));
   }
   elseif (is_day()) {
-      $dwooParams['archive']['searchterm'] = get_the_date(__('j. F  Y'));
+      $searchterm = get_the_date(__('j. F  Y'));
   }
   elseif (is_time()) {
-      $dwooParams['archive']['searchterm'] = get_the_time(__('j. F Y, H:i \U\h\r'));
+      $searchterm = get_the_time(__('j. F Y, H:i \U\h\r'));
   }
 
-  $dwooParams['archive']['is_year'] = is_year();
-  $dwooParams['archive']['is_month'] = is_month();
-  $dwooParams['archive']['is_day'] = is_day();
-  $dwooParams['archive']['is_time'] = is_time();
-
-  $dwoo->output(TPL_PATH.'/date.tpl', $dwooParams);
+?>
+  
+<div id="searchresult_header" class="searchresult_header">
+<h2 class="searchresult_header">Ergebnisse der 
+    <?php if(is_year()): ?>Jahres-<?php elseif(is_month()): ?>Monats-<?php elseif(is_day()): ?>Tages-<?php elseif(is_time()): ?>Zeit-<?php endif; ?>Archivsuche</h2>
+<p><?php if(is_year()): ?>
+    Du hast den Blog nach dem Jahr <strong>{$archive.searchterm}</strong> durchsucht.
+    <?php elseif(is_month()): ?>
+    Du hast den Blog nach dem Monat <strong>{$archive.searchterm}</strong> durchsucht.
+    <?php elseif(is_day()): ?>
+    Du hast den Blog nach dem <strong>{$archive.searchterm}</strong> durchsucht.
+    <?php elseif(is_time()): ?>
+    Du hast den Blog nach der Uhrzeit <strong>{$archive.searchterm}</strong> durchsucht.
+    Respekt dafür.
+    <?php endif; ?>
+    Hier ist, was sich finden ließ.</p>
+</div>
+<div class="separate"></div>
+  
+  <?php
+  
   include(TEMPLATEPATH.'/theloop.php');
 
   get_footer();
