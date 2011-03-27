d27ac34b51ce4f2babff4938ea8f6f752871709b
Markus Mayer <development@widemeadows.de>
2011-03-27 20:46:18 +0200

    Tagarchiv

--- tag.php
+++ tag.php
@@ -4,9 +4,18 @@
 
   get_header();
 
-  $dwooParams['archive']['searchterm'] = single_tag_title("", FALSE);
+  $searchterm = single_tag_title("", FALSE);
 
-  $dwoo->output(TPL_PATH.'/tag.tpl', $dwooParams);
+  ?>
+  
+ div id="searchresult_header" class="searchresult_header">
+<h2 class="searchresult_header">Schlüsselwortarchiv</h2>
+<p>Du betrachtest das Archiv des Tags <strong><?php echo $searchterm; ?></strong>.</p>
+</div>
+<div class="separate"></div>
+  
+  <?php
+    
   include(TEMPLATEPATH.'/theloop.php');
 
   get_footer();
